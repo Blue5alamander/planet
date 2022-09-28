b472f372d07be8bdc50950259f4e23708b26b124
Kirit Sælensminde <kirit@kirit.com>
2022-09-28 21:32:07 +0700

    Start to add in command handler

--- src/connection.cpp
+++ src/connection.cpp
@@ -1,15 +1,24 @@
 #include <planet/connection.hpp>
 
+#include <iostream>
+
 
 felspar::coro::stream<std::string> planet::io::connection(
         felspar::coro::stream<felspar::coro::generator<std::string_view>>
                 commands) {
     while (auto command = co_await commands.next()) {
         std::string expr;
-        for (auto part : *command) {
-            if (not expr.empty()) { expr += ' '; }
-            expr += part;
+        auto const first = command->next();
+        if (not first || first->empty()) {
+            // blank line
+        } else if (first == "print") {
+            for (auto part : *command) {
+                if (not expr.empty()) { expr += ' '; }
+                expr += part;
+            }
+            co_yield std::move(expr);
+        } else {
+            std::cout << "Unknown command '" << *first << "' -- ignoring\n";
         }
-        co_yield std::move(expr);
     }
 }
