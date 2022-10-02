c7c81bf70c784af78ec7f29c7c1c60f15bde281a
Kirit Sælensminde <kirit@kirit.com>
2022-10-02 17:19:09 +0700

    Include an overloaded implementation for visiting the variant

--- include/planet/message.hpp
+++ include/planet/message.hpp
@@ -10,8 +10,20 @@ namespace planet::client {
     };
 
 
+    template<class... Ts>
+    struct overloaded : Ts... {
+        using Ts::operator()...;
+    };
+    template<class... Ts>
+    overloaded(Ts...) -> overloaded<Ts...>;
+
     struct message {
         std::variant<std::string, error> payload;
+
+        template<typename... Funcs>
+        auto operator()(Funcs &&...fs) -> decltype(auto) {
+            return std::visit(overloaded{std::forward<Funcs>(fs)...}, payload);
+        }
     };
 
 
