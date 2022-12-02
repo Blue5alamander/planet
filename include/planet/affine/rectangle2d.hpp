fc58d955b443b1ab08b156dc70f6cc13dbb2738a
Kirit Sælensminde <kirit@felspar.com>
2022-12-02 14:56:00 +0700

    Extend rectangle API with useful members

--- include/planet/affine/rectangle2d.hpp
+++ include/planet/affine/rectangle2d.hpp
@@ -17,10 +17,15 @@ namespace planet::affine {
         constexpr rectangle(point2d const tl, point2d const br)
         : top_left{tl}, extents{br - tl} {}
 
+        point2d bottom_right() const noexcept { return top_left + extents; }
         float top() const noexcept { return top_left.y(); }
         float left() const noexcept { return top_left.x(); }
         float bottom() const noexcept { return top() + extents.height; }
         float right() const noexcept { return left() + extents.width; }
+
+        friend rectangle operator+(rectangle const r, point2d const p) {
+            return {r.top_left + p, r.extents};
+        }
     };
 
 
