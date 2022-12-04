8c366bbf53d31f7d4bcb2deb3fbe18a9a348c677
Kirit Sælensminde <kirit@felspar.com>
2022-12-04 14:59:30 +0700

    Grid layout can now be over a tuple of content

--- include/planet/ui/layout.grid.hpp
+++ include/planet/ui/layout.grid.hpp
@@ -3,6 +3,8 @@
 
 #include <planet/affine2d.hpp>
 
+#include <felspar/memory/small_vector.hpp>
+
 
 namespace planet::ui {
 
@@ -11,7 +13,6 @@ namespace planet::ui {
     template<typename C>
     struct grid {
         using collection_type = C;
-        using box_type = typename collection_type::value_type;
         collection_type items;
         /// Padding between items in the row
         float vpadding = {}, hpadding = {};
@@ -50,6 +51,57 @@ namespace planet::ui {
             float max_width = {}, max_height = {};
             for (auto const &i : items) {
                 auto const ex = i.extents(outer);
+                max_width = std::max(ex.width, max_width);
+                max_height = std::max(ex.height, max_height);
+            }
+            return {max_width, max_height};
+        }
+    };
+    template<typename... Pack>
+    struct grid<std::tuple<Pack...>> {
+        using collection_type = std::tuple<Pack...>;
+        collection_type items;
+        /// Padding between items in the row
+        float vpadding = {}, hpadding = {};
+
+        grid(collection_type c, float const p)
+        : items{std::move(c)}, vpadding{p}, hpadding{p} {}
+
+        affine::extents2d extents(affine::extents2d const outer) const {
+            auto const cell = cell_size(outer);
+            auto const w = std::floor(
+                    (outer.width + hpadding) / (cell.width + hpadding));
+            auto const h = std::floor(sizeof...(Pack) / w);
+            return {w * cell.width + (w - 1) * hpadding,
+                    h * cell.height + (h - 1) * vpadding};
+        }
+
+        template<typename Target>
+        void draw_within(Target &t, affine::rectangle const within) {
+            auto const cell = cell_size(within.extents);
+            float x = {}, y = {};
+            felspar::memory::small_vector<affine::rectangle, sizeof...(Pack)>
+                    locations;
+            for (std::size_t index{}; index < sizeof...(Pack); ++index) {
+                if (x > 0 and x + cell.width > within.extents.width) {
+                    x = 0;
+                    if (y) { y += vpadding; }
+                    y += cell.height;
+                }
+                locations.emplace_back(
+                        affine::point2d{x, y} + within.top_left, cell);
+                if (x) { x += hpadding; }
+                x += cell.width;
+            }
+            draw_items_within(
+                    t, items, locations,
+                    std::make_index_sequence<sizeof...(Pack)>{});
+        }
+
+      private:
+        affine::extents2d cell_size(affine::extents2d const outer) const {
+            float max_width = {}, max_height = {};
+            for (auto const &ex : item_sizes(items, outer)) {
                 max_width = std::max(ex.width, max_width);
                 max_height = std::max(ex.height, max_height);
             }
