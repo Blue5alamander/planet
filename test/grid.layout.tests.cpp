5eacc199e9500e79e2fda1bd35e6023c38fce974
Kirit Sælensminde <kirit@kirit.com>
2022-12-09 11:15:04 +0700

    Test some more combinations

--- test/grid.layout.tests.cpp
+++ test/grid.layout.tests.cpp
@@ -10,20 +10,39 @@ namespace {
 
 
     auto const rows1 = suite.test("one row", [](auto check, auto &log) {
+        auto r1 = planet::ui::grid{
+                std::array{planet::debug::ui_element{{3, 4}}}, {}};
+        check(r1.extents({10, 10}).width) == 3.0f;
+        check(r1.extents({10, 10}).height) == 4.0f;
+
+
         auto r2 = planet::ui::grid{
                 std::array{
                         planet::debug::ui_element{{3, 4}},
                         planet::debug::ui_element{{3, 4}}},
                 {}};
-
         check(r2.extents({10, 10}).width) == 6.0f;
         check(r2.extents({10, 10}).height) == 4.0f;
 
         r2.hpadding = 2.0f;
         r2.vpadding = 3.0f;
-
         check(r2.extents({10, 10}).width) == 8.0f;
         check(r2.extents({10, 10}).height) == 4.0f;
+
+
+        auto r3 = planet::ui::grid{
+                std::array{
+                        planet::debug::ui_element{{3, 4}},
+                        planet::debug::ui_element{{3, 4}},
+                        planet::debug::ui_element{{3, 4}}},
+                {}};
+        check(r3.extents({10, 10}).width) == 9.0f;
+        check(r3.extents({10, 10}).height) == 4.0f;
+
+        r3.hpadding = 1.0f;
+        r3.vpadding = 3.0f;
+        check(r3.extents({11, 10}).width) == 11.0f;
+        check(r3.extents({11, 10}).height) == 4.0f;
     });
 
 
