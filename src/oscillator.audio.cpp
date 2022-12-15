5859e76f0fdbfee459b34c3ce130df93a0a8bd0a
Kirit Sælensminde <kirit@felspar.com>
2022-12-15 08:07:46 +0700

    Use the planet/numbers.hpp header

--- src/oscillator.audio.cpp
+++ src/oscillator.audio.cpp
@@ -1,17 +1,8 @@
 #include <planet/audio/clocks.hpp>
 #include <planet/audio/oscillator.hpp>
+#include <planet/numbers.hpp>
 
 #include <complex>
-#if __has_include(<numbers>)
-#include <numbers>
-namespace {
-    constexpr auto tau = std::numbers::pi_v<float> * 2.0f;
-}
-#else
-namespace {
-    constexpr auto tau = 6.283185307179586f;
-}
-#endif
 
 
 felspar::coro::generator<std::span<float>> silence() {
@@ -23,7 +14,7 @@ felspar::coro::generator<std::span<float>> silence() {
 felspar::coro::generator<std::span<float>>
         planet::audio::oscillator(float const turns) {
     std::array<float, planet::audio::default_buffer_samples> buffer;
-    std::complex const rotate{std::cos(turns * tau), std::sin(turns * tau)};
+    std::complex const rotate{std::cos(turns * τ), std::sin(turns * τ)};
     std::complex phase{1.0f, 0.0f};
     while (true) {
         for (auto &s : buffer) {
