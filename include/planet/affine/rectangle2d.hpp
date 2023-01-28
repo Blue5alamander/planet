14b371e6efc4b00327dc94ac4decfc7238128222
Kirit Sælensminde <kirit@felspar.com>
2023-01-28 12:57:08 +0700

    Add a `contains` member to the rectangle

--- include/planet/affine/rectangle2d.hpp
+++ include/planet/affine/rectangle2d.hpp
@@ -24,6 +24,14 @@ namespace planet::affine {
         float bottom() const noexcept { return top() + extents.height; }
         float right() const noexcept { return left() + extents.width; }
 
+        bool contains(point2d const p) const {
+            return p.x() >= left() and p.x() <= right() and p.y() >= top()
+                    and p.y() <= bottom();
+        }
+        bool contains(rectangle2d const r) const {
+            return contains(r.top_left) and contains(r.bottom_right());
+        }
+
         /// ### Move the rectangle
         friend rectangle2d operator+(rectangle2d const r, point2d const p) {
             return {r.top_left + p, r.extents};
