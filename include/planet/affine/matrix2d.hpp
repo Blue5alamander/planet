e9d266fb096dcd1c7b924af41a6ab3b19ff83a2a
Kirit Sælensminde <kirit@felspar.com>
2023-01-29 14:52:53 +0700

    Rename matrix to be 2d matrix

--- include/planet/affine/matrix2d.hpp
+++ include/planet/affine/matrix2d.hpp
@@ -9,11 +9,11 @@
 namespace planet::affine {
 
 
-    /// 2D matrix used for affine transformations
-    class matrix final {
+    /// ## 2D affine transform matrix
+    class matrix2d final {
         std::array<float, 9> m = {1, 0, 0, 0, 1, 0, 0, 0, 1};
 
-        constexpr matrix(
+        constexpr matrix2d(
                 float const a,
                 float const b,
                 float const c,
@@ -26,10 +26,11 @@ namespace planet::affine {
         : m{a, b, c, d, e, f, g, h, i} {}
 
       public:
-        constexpr matrix() {}
+        constexpr matrix2d() {}
 
-        friend constexpr matrix operator*(matrix const &a, matrix const &b) {
-            return matrix{
+        friend constexpr matrix2d
+                operator*(matrix2d const &a, matrix2d const &b) {
+            return matrix2d{
                     a.m[0] * b.m[0] + a.m[1] * b.m[3] + a.m[2] * b.m[6],
                     a.m[0] * b.m[1] + a.m[1] * b.m[4] + a.m[2] * b.m[7],
                     a.m[0] * b.m[2] + a.m[1] * b.m[5] + a.m[2] * b.m[8],
@@ -42,28 +43,28 @@ namespace planet::affine {
                     a.m[6] * b.m[1] + a.m[7] * b.m[4] + a.m[8] * b.m[7],
                     a.m[6] * b.m[2] + a.m[7] * b.m[5] + a.m[8] * b.m[8]};
         }
-        friend constexpr point2d operator*(matrix const &a, point2d const p) {
+        friend constexpr point2d operator*(matrix2d const &a, point2d const p) {
             return {a.m[0] * p.xh + a.m[1] * p.yh + a.m[2] * p.h,
                     a.m[3] * p.xh + a.m[4] * p.yh + a.m[5] * p.h,
                     a.m[6] * p.xh + a.m[7] * p.yh + a.m[8] * p.h};
         }
 
         friend constexpr bool
-                operator==(matrix const &, matrix const &) = default;
+                operator==(matrix2d const &, matrix2d const &) = default;
 
         /// Reflect the y axis (about the x axis)
-        static constexpr matrix reflect_y() {
-            return matrix{1, 0, 0, 0, -1, 0, 0, 0, 1};
+        static constexpr matrix2d reflect_y() {
+            return matrix2d{1, 0, 0, 0, -1, 0, 0, 0, 1};
         }
-        static constexpr matrix translate(point2d const by) {
-            return matrix{1, 0, by.x(), 0, 1, by.y(), 0, 0, 1};
+        static constexpr matrix2d translate(point2d const by) {
+            return matrix2d{1, 0, by.x(), 0, 1, by.y(), 0, 0, 1};
         }
-        static constexpr matrix scale(float const factor) {
-            return matrix{factor, 0, 0, 0, factor, 0, 0, 0, 1};
+        static constexpr matrix2d scale(float const factor) {
+            return matrix2d{factor, 0, 0, 0, factor, 0, 0, 0, 1};
         }
-        static constexpr matrix rotate(float const turns) {
+        static constexpr matrix2d rotate(float const turns) {
             float const r = turns * τ;
-            return matrix{
+            return matrix2d{
                     std::cos(r),
                     -std::sin(r),
                     0,
--- include/planet/affine/transform2d.hpp
+++ include/planet/affine/transform2d.hpp
@@ -9,31 +9,31 @@ namespace planet::affine {
 
     /// 2D Affine transform into and out of a co-ordinate space
     class transform final {
-        matrix in, out;
+        matrix2d in, out;
 
       public:
         /// Reflect the y-axis, so up is down
         transform &reflect_y() {
-            in = matrix::reflect_y() * in;
-            out = out * matrix::reflect_y();
+            in = matrix2d::reflect_y() * in;
+            out = out * matrix2d::reflect_y();
             return *this;
         }
         /// Scale up by the provided factor
         transform &scale(float const factor) {
-            in = matrix::scale(factor) * in;
-            out = out * matrix::scale(1.0f / factor);
+            in = matrix2d::scale(factor) * in;
+            out = out * matrix2d::scale(1.0f / factor);
             return *this;
         }
         /// Translate by the provided amount
         transform &translate(point2d const by) {
-            in = matrix::translate(by) * in;
-            out = out * matrix::translate(-by);
+            in = matrix2d::translate(by) * in;
+            out = out * matrix2d::translate(-by);
             return *this;
         }
         /// Rotate by the requested number of turns. One turn is τ radians
         transform &rotate(float const turns) {
-            in = matrix::rotate(turns) * in;
-            out = out * matrix::rotate(-turns);
+            in = matrix2d::rotate(turns) * in;
+            out = out * matrix2d::rotate(-turns);
             return *this;
         }
 
