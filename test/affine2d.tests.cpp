324ea461bc2bfef512e0ba6a4ae27ffb7a9e647b
Kirit Sælensminde <kirit@felspar.com>
2023-01-30 10:26:40 +0700

    clang-format

--- test/affine2d.tests.cpp
+++ test/affine2d.tests.cpp
@@ -138,12 +138,12 @@ namespace {
             });
 
 
-            auto const mat3 = felspar::testsuite("affine3d/matrix", [](auto check) {
-                auto const tr = planet::affine::matrix3d::translate(1, 2, 3);
-                check(tr[{3, 0}]) == 1.0f;
-                check(tr[{3, 1}]) == 2.0f;
-                check(tr[{3, 2}]) == 3.0f;
-            });
+    auto const mat3 = felspar::testsuite("affine3d/matrix", [](auto check) {
+        auto const tr = planet::affine::matrix3d::translate(1, 2, 3);
+        check(tr[{3, 0}]) == 1.0f;
+        check(tr[{3, 1}]) == 2.0f;
+        check(tr[{3, 2}]) == 3.0f;
+    });
 
 
 }
