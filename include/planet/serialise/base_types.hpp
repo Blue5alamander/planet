2a0484f2dda8e53b013d38d9057265da8a05320b
Kirit Sælensminde <kirit@felspar.com>
2023-02-15 20:48:31 +0700

    Remove save/load support for some types as they're probably a bad idea

--- include/planet/serialise/base_types.hpp
+++ include/planet/serialise/base_types.hpp
@@ -53,50 +53,6 @@ namespace planet::serialise {
     }
 
 
-    inline save_buffer &save(save_buffer &ab, std::string_view const sv) {
-        ab.append(sv);
-        return ab;
-    }
-    inline void load(load_buffer &l, std::string_view &sv) {
-        auto bytes = load_type<std::size_t>(l);
-        sv = {reinterpret_cast<char const *>(l.split(bytes).data()), bytes};
-    }
-
-
-    template<typename P>
-    inline save_buffer &save(save_buffer &ab, std::unique_ptr<P> const &p) {
-        if (p) {
-            return ab.save_box("_s:uqp:v", *p);
-        } else {
-            return ab.save_box("_s:uqp:e");
-        }
-    }
-    template<typename P>
-    inline void load(load_buffer &l, std::unique_ptr<P> &p) {
-        auto b = load_type<box>(l);
-        if (b.name == "_s:uqp:v") {
-            p = std::make_unique<P>(load_type<P>(b.content));
-        } else if (b.name == "_s:uqp:e") {
-            p.reset();
-        } else {
-            throw felspar::stdexcept::runtime_error{"Unexpected box name"};
-        }
-    }
-
-
-    template<typename F, typename S>
-    inline save_buffer &save(save_buffer &ab, std::pair<F, S> const &p) {
-        save(ab, p.first);
-        save(ab, p.second);
-        return ab;
-    }
-    template<typename F, typename S>
-    inline void load(load_buffer &l, std::pair<F, S> &p) {
-        load(l, p.first);
-        load(l, p.second);
-    }
-
-
     template<typename T, std::size_t N>
     inline save_buffer &save(save_buffer &ab, std::span<T, N> const a) {
         ab.append(a.size());
