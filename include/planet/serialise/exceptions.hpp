4509a44301468f662917e68bf6cc425dc9d5974d
Kirit Sælensminde <kirit@felspar.com>
2023-02-17 16:11:49 +0700

    Add hexdumps to some exceptions to help debugging

--- include/planet/serialise/exceptions.hpp
+++ include/planet/serialise/exceptions.hpp
@@ -5,6 +5,8 @@
 
 #include <felspar/exceptions.hpp>
 
+#include <span>
+
 
 namespace planet::serialise {
 
@@ -14,8 +16,19 @@ namespace planet::serialise {
 
     /// ### Super-class for all serialisation errors
     class serialisation_error : public felspar::stdexcept::runtime_error {
-      public:
+        std::string what_message, hexdump;
+        char const *what() const noexcept override;
+
+      protected:
         serialisation_error(std::string, felspar::source_location const &);
+        serialisation_error(
+                std::string,
+                std::span<std::byte const>,
+                felspar::source_location const &);
+
+      public:
+        /// This happened inside the given box
+        void inside_box(std::string_view);
     };
 
 
@@ -29,10 +42,18 @@ namespace planet::serialise {
     };
 
 
-    /// ### A box still has data after reading
+    /// ### A box still has data after reading, or the box is not large enough
     class box_not_empty : public serialisation_error {
       public:
         box_not_empty(
+                felspar::source_location const & =
+                        felspar::source_location::current());
+    };
+    class buffer_not_big_enough : public serialisation_error {
+      public:
+        buffer_not_big_enough(
+                std::size_t wanted,
+                std::size_t got,
                 felspar::source_location const & =
                         felspar::source_location::current());
     };
@@ -42,6 +63,7 @@ namespace planet::serialise {
     class wanted_boolean : public serialisation_error {
       public:
         wanted_boolean(
+                std::span<std::byte const>,
                 marker got,
                 felspar::source_location const & =
                         felspar::source_location::current());
@@ -49,6 +71,7 @@ namespace planet::serialise {
     class wrong_marker : public serialisation_error {
       public:
         wrong_marker(
+                std::span<std::byte const>,
                 marker exected,
                 marker got,
                 felspar::source_location const & =
--- src/serialise.cpp
+++ src/serialise.cpp
@@ -2,6 +2,8 @@
 #include <planet/serialise/load.hpp>
 #include <planet/serialise/save_buffer.hpp>
 
+#include <felspar/memory/hexdump.hpp>
+
 #include <cstring>
 #include <ostream>
 #include <string>
@@ -27,12 +29,13 @@ void planet::serialise::box::check_empty_or_throw(
 /// ## `planet::serialise::load_buffer`
 
 
-std::size_t planet::serialise::load_buffer::extract_size_t() {
-    auto const bytes = extract<std::uint64_t>();
+std::size_t planet::serialise::load_buffer::extract_size_t(
+        felspar::source_location const &loc) {
+    auto const bytes = extract<std::uint64_t>(loc);
     if constexpr (sizeof(std::size_t) < sizeof(std::uint64_t)) {
         if (not std::in_range<std::size_t>(bytes)) {
             throw felspar::stdexcept::runtime_error{
-                    "This save file is too large to load on this machine"};
+                    "This save file is too large to load on this machine", loc};
         } else {
             return std::size_t(bytes);
         }
@@ -117,18 +120,52 @@ std::string_view planet::serialise::to_string(marker const m) {
 }
 
 
-/// ## `planet::serialise::serialisation_error` and related sub-classes
+/// ## `planet::serialise::serialisation_error`
 
 
 planet::serialise::serialisation_error::serialisation_error(
         std::string m, felspar::source_location const &loc)
 : felspar::stdexcept::runtime_error{std::move(m), loc} {}
 
+planet::serialise::serialisation_error::serialisation_error(
+        std::string m,
+        std::span<std::byte const> r,
+        felspar::source_location const &loc)
+: felspar::stdexcept::runtime_error{std::move(m), loc},
+  hexdump{felspar::memory::hexdump(r)} {}
+
+char const *planet::serialise::serialisation_error::what() const noexcept {
+    if (what_message.empty()) {
+        return felspar::stdexcept::runtime_error::what();
+    } else {
+        return what_message.c_str();
+    }
+}
+
+void planet::serialise::serialisation_error::inside_box(std::string_view sv) {
+    if (what_message.empty()) {
+        what_message = std::string{felspar::stdexcept::runtime_error::what()};
+        if (not hexdump.empty()) {
+            what_message.append("\nRemaining buffer: ");
+            what_message.append(hexdump);
+        }
+        what_message.append("\nBox path: ");
+        what_message.append(sv);
+    } else {
+        what_message.append(" <- ");
+        what_message.append(sv);
+    }
+}
+
+
+/// ## `planet::serialise::serialisation_error` sub-classes
+
 
 planet::serialise::box_name_length::box_name_length(
         std::string_view box_name, felspar::source_location const &loc)
 : serialisation_error{
-        std::string{"The box name must be between 1 and 127 bytes long '"}
+        std::string{"The box name must be between 1 and 127 bytes long\n"
+                    "Name tried is '"}
                 + std::string{box_name} + '\'',
         loc} {}
 
@@ -139,23 +176,38 @@ planet::serialise::box_not_empty::box_not_empty(
         "The box was not empty after loading all data from it", loc} {}
 
 
-planet::serialise::wanted_boolean::wanted_boolean(
-        marker const m, felspar::source_location const &loc)
+planet::serialise::buffer_not_big_enough::buffer_not_big_enough(
+        std::size_t const wanted,
+        std::size_t const got,
+        felspar::source_location const &loc)
 : serialisation_error{
-        std::string{"Expected b_true or b_false for a boolean, but "
-                    "got "}
-                + std::string{to_string(m)},
+        "There was not enough data in the buffer to read the requested value\n"
+        "Wanted "
+                + std::to_string(wanted) + " bytes and buffer only contains "
+                + std::to_string(got) + " bytes",
         loc} {}
 
 
+planet::serialise::wanted_boolean::wanted_boolean(
+        std::span<std::byte const> remaining,
+        marker const m,
+        felspar::source_location const &loc)
+: serialisation_error{
+        std::string{"Expected b_true or b_false for a boolean\n"
+                    "Got "}
+                + std::string{to_string(m)},
+        remaining, loc} {}
+
+
 planet::serialise::wrong_marker::wrong_marker(
+        std::span<std::byte const> remaining,
         marker const expected,
         marker const got,
         felspar::source_location const &loc)
 : serialisation_error{
-        std::string{"The wrong type marker is in the save file. "
+        std::string{"The wrong type marker is in the save file\n"
                     "Expected "}
                 + std::string{to_string(expected)} + " and got "
                 + std::string{to_string(got)} + " ("
                 + std::to_string(static_cast<std::uint8_t>(got)) + ")",
-        loc} {}
+        remaining, loc} {}
