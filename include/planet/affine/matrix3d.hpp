cc7a8846c98d8f67b158bafcc916725e08bccfb1
Kirit Sælensminde <kirit@felspar.com>
2023-02-26 15:51:41 +0700

    Make the 3d matrix more constexpr and more safe

--- include/planet/affine/matrix3d.hpp
+++ include/planet/affine/matrix3d.hpp
@@ -23,7 +23,7 @@ namespace planet::affine {
                                                0, 0, 1, 0, 0, 0, 0, 1};
 
         /// Construct the matrix from a span of 4 rows.
-        matrix3d(std::array<std::array<float, 4>, 4> const &v)
+        constexpr matrix3d(std::array<std::array<float, 4>, 4> const &v)
         : m{v[0][0], v[1][0], v[2][0], v[3][0],
 
             v[0][1], v[1][1], v[2][1], v[3][1],
@@ -33,20 +33,22 @@ namespace planet::affine {
             v[0][3], v[1][3], v[2][3], v[3][3]} {}
 
       public:
-        matrix3d() {}
+        constexpr matrix3d() {}
 
-        float operator[](std::pair<std::size_t, std::size_t> const s) const {
-            return m[s.first * 4 + s.second];
+        constexpr float operator[](
+                std::pair<std::size_t, std::size_t> const s) const noexcept {
+            return m[(s.first bitand 3) * 4 + (s.second bitand 3)];
         }
 
-        static matrix3d translate(float const x, float const y, float const z) {
+        static constexpr matrix3d
+                translate(float const x, float const y, float const z) {
             return {
                     {std::array{1.0f, 0.0f, 0.0f, x},
                      std::array{0.0f, 1.0f, 0.0f, y},
                      std::array{0.0f, 0.0f, 1.0f, z},
                      std::array{0.0f, 0.0f, 0.0f, 1.0f}}};
         }
-        static matrix3d scale_xy(float const x, float const y) {
+        static constexpr matrix3d scale_xy(float const x, float const y) {
             return {
                     {std::array{x, 0.0f, 0.0f, 0.0f},
                      std::array{0.0f, y, 0.0f, 0.0f},
