c43d8ed120eebf3aeffad7b31bc0968fa6f407ee
Kirit Sælensminde <kirit@felspar.com>
2023-02-27 12:03:23 +0700

    Fix up some comments

--- include/planet/ui/panel.hpp
+++ include/planet/ui/panel.hpp
@@ -11,11 +11,11 @@
 namespace planet {
 
 
-    /// Co-ordinate transform hierarchy for world etc. co-ordinate systems
+    /// ## Co-ordinate transform hierarchy co-ordinate spaces
     class panel final {
         panel *parent = nullptr;
 
-        /// Transformation into and out of the coordinate space
+        /// ### Transformation into and out of the local coordinate space
         affine::transform2d viewport = {};
 
         struct child final {
@@ -32,12 +32,15 @@ namespace planet {
         void reparent_children(panel *);
 
       public:
+        /// ### Construction and destruction
         panel();
         ~panel();
 
-        /// Given the way we want to be able to use panels, we have to be able
-        /// to ensure stable addresses, so: not copyable for sure, and never
-        /// movable once there are children
+        /**
+         * Given the way we want to be able to use panels, we have to be able to
+         * ensure stable addresses, so: not copyable for sure, and never movable
+         * once there are children.
+         */
         panel(panel const &) = delete;
         panel(panel &&,
               felspar::source_location const & =
@@ -46,16 +49,18 @@ namespace planet {
         panel &operator=(panel &&) = delete;
 
 
-        /// ## Co-ordinate space for this panel
+        /// ### Co-ordinate space for this panel
 
-        /// Remove all transformations into and out of the coordinate space
+        /// #### Remove all transformations into and out of the coordinate space
         void reset_coordinate_space() { viewport = {}; }
 
-        /// Forward to the viewport taking the hierarchy into account. When
-        /// going into the screen co-ordinate space we must apply our local
-        /// transformations before we apply the parent ones. When coming out of
-        /// the screen coordinates we have to undo the parent transformation
-        /// first
+        /// #### In and out of the co-ordinate space
+        /**
+         * Takes the panel hierarchy into account. When going into the screen
+         * co-ordinate space we must apply our local transformations before we
+         * apply the parent ones. When coming out of the screen coordinates we
+         * have to undo the parent transformation first
+         */
         affine::point2d into(affine::point2d const p) {
             if (parent) {
                 return parent->into(viewport.into(p));
@@ -71,6 +76,7 @@ namespace planet {
             }
         }
 
+        /// #### Transformations of the local co-ordinate space
         panel &reflect_y() {
             viewport.reflect_y();
             return *this;
@@ -89,14 +95,17 @@ namespace planet {
         }
 
 
-        /// ## Panel hierarchy management
+        /// ### Panel hierarchy management
 
-        /// Tell if this parent currently has a parent or not
+        /// #### Tell if this parent currently has a parent or not
         bool has_parent() const noexcept { return parent; }
-        /// Add to a parent, but don't position it
+        /// #### Add to a parent, but don't position it
         void add_child(panel &);
-        /// Add a child covering the requested portion of the parent in the
-        /// panel's coordinate space
+        /// #### Add a child
+        /**
+         * The child covers the requested portion of the parent in this panel's
+         * coordinate space
+         */
         void add_child(panel &, affine::rectangle2d);
         void add_child(
                 panel &p,
@@ -104,25 +113,25 @@ namespace planet {
                 affine::point2d const bottom_right) {
             add_child(p, {top_left, bottom_right});
         }
-        /// Remove the passed child from the panel
+        /// #### Remove the passed child from the panel
         void remove_child(panel &);
 
 
-        /// ## Panel positioning
+        /// ### Panel positioning
 
-        /// Move the location of this panel in the parent's space
+        /// #### Move the location of this panel in the parent's space
         void move_to(affine::rectangle2d);
 
 
-        /// ## Message delivery and focus
+        /// ### Message delivery and focus
 
-        /// Left button release position
+        /// #### Mouse clicks in the local co-ordinate space
         felspar::coro::bus<planet::events::click> clicks;
 
 
-        /// ## Drawing in the panel coordinate space
+        /// ### Drawing in the panel coordinate space
 
-        /// Draw a line between two points in world co-ordinate space
+        /// #### Draw a line between two points in world co-ordinate space
         template<typename R>
         void
                 line(R &renderer,
@@ -132,14 +141,15 @@ namespace planet {
             auto const p2 = viewport.into(cp2);
             renderer.line(p1.x(), p1.y(), p2.x(), p2.y());
         }
-        /// Draw the texture at native size with its top left corder at the
-        /// location specified
+        /// #### Draw the texture
+        /// At native size with its top left corner at the location specified
         template<typename R, typename T>
         void copy(R &renderer, T const &tex, affine::point2d const l) const {
             auto const p = viewport.into(l);
             renderer.copy(tex, p.x(), p.y());
         }
-        /// Scale the texture, drawing it to fill the rectangle
+        /// #### Scale the texture
+        /// The texture fills the rectangle
         template<typename R, typename T>
         void copy(R &renderer, T const &tex, affine::rectangle2d const r) const {
             auto const tl = viewport.into(r.top_left);
