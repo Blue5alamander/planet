2d50ac31bd904843a0423f63b6df5274fdc0e232
Kirit Sælensminde <kirit@felspar.com>
2023-02-28 12:22:15 +0700

    Add simple 4x4 matrix multiply

--- include/planet/affine/matrix3d.hpp
+++ include/planet/affine/matrix3d.hpp
@@ -62,6 +62,65 @@ namespace planet::affine {
                 std::pair<std::size_t, std::size_t> const s) const noexcept {
             return m[(s.first bitand 3) * 4 + (s.second bitand 3)];
         }
+
+        friend constexpr matrix3d
+                operator*(matrix3d const &a, matrix3d const &b) {
+            float const a0 = a.m[0], a1 = a.m[1], a2 = a.m[2], a3 = a.m[3],
+
+                        a4 = a.m[4], a5 = a.m[5], a6 = a.m[6], a7 = a.m[7],
+
+                        a8 = a.m[8], a9 = a.m[9], a10 = a.m[10], a11 = a.m[11],
+
+                        a12 = a.m[12], a13 = a.m[13], a14 = a.m[14],
+                        a15 = a.m[15];
+
+            float const b0 = b.m[0], b1 = b.m[1], b2 = a.m[2], b3 = b.m[3],
+
+                        b4 = b.m[4], b5 = b.m[5], b6 = b.m[6], b7 = b.m[7],
+
+                        b8 = b.m[8], b9 = b.m[9], b10 = b.m[10], b11 = b.m[11],
+
+                        b12 = b.m[12], b13 = b.m[13], b14 = b.m[14],
+                        b15 = a.m[15];
+
+            return {
+                    {std::array<float, 4>{
+                             a0 * b0 + a4 * b1 + a8 * b2 + a12 * b3,
+
+                             a0 * b4 + a4 * b5 + a8 * b6 + a12 * b7,
+
+                             a0 * b8 + a4 * b9 + a8 * b10 + a12 * b11,
+
+                             a0 * b12 + a4 * b13 + a8 * b14 + a12 * b15,
+                     },
+                     std::array{
+                             a1 * b0 + a5 * b1 + a9 * b2 + a13 * b3,
+
+                             a1 * b4 + a5 * b5 + a9 * b6 + a13 * b7,
+
+                             a1 * b8 + a5 * b9 + a9 * b10 + a13 * b11,
+
+                             a1 * b12 + a5 * b13 + a9 * b14 + a13 * b15,
+                     },
+                     std::array{
+                             a2 * b0 + a6 * b1 + a10 * b2 + a14 * b3,
+
+                             a2 * b4 + a6 * b5 + a10 * b6 + a14 * b7,
+
+                             a2 * b8 + a6 * b9 + a10 * b10 + a14 * b11,
+
+                             a2 * b12 + a6 * b13 + a10 * b14 + a14 * b15,
+                     },
+                     std::array{
+                             a3 * b0 + a7 * b1 + a11 * b2 + a15 * b3,
+
+                             a3 * b4 + a7 * b5 + a11 * b6 + a15 * b7,
+
+                             a3 * b8 + a7 * b9 + a11 * b10 + a15 * b11,
+
+                             a3 * b12 + a7 * b13 + a11 * b14 + a15 * b15,
+                     }}};
+        }
     };
 
 
--- test/affine3d.tests.cpp
+++ test/affine3d.tests.cpp
@@ -16,6 +16,25 @@ namespace {
         check(t2[{3, 0}]) == 2.0f;
         check(t2[{3, 1}]) == 3.0f;
         check(t2[{3, 2}]) == 0.0f;
+
+        auto const t3 = planet::affine::matrix3d::translate(2, 3, 4);
+        auto const mul = planet::affine::matrix3d{} * t3;
+        check(mul[{0, 0}]) == t3[{0, 0}];
+        check(mul[{1, 0}]) == t3[{1, 0}];
+        check(mul[{2, 0}]) == t3[{2, 0}];
+        check(mul[{3, 0}]) == t3[{3, 0}];
+        check(mul[{0, 1}]) == t3[{0, 1}];
+        check(mul[{1, 1}]) == t3[{1, 1}];
+        check(mul[{2, 1}]) == t3[{2, 1}];
+        check(mul[{3, 1}]) == t3[{3, 1}];
+        check(mul[{0, 2}]) == t3[{0, 2}];
+        check(mul[{1, 2}]) == t3[{1, 2}];
+        check(mul[{2, 2}]) == t3[{2, 2}];
+        check(mul[{3, 2}]) == t3[{3, 2}];
+        check(mul[{0, 3}]) == t3[{0, 3}];
+        check(mul[{1, 3}]) == t3[{1, 3}];
+        check(mul[{2, 3}]) == t3[{2, 3}];
+        check(mul[{3, 3}]) == t3[{3, 3}];
     });
 
 
