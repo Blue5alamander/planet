86d9859a621e0d78a37b5984ea2995e708c9fcb9
Kirit Sælensminde <kirit@felspar.com>
2023-04-01 18:43:29 +0700

    Skip the grid member and type for now

--- include/planet/ui/layout.hpp
+++ include/planet/ui/layout.hpp
@@ -8,13 +8,9 @@ namespace planet::ui {
 
 
     /// ## A layout
-    template<
-            typename T,
-            typename C = std::vector<element<T>>,
-            typename G = std::vector<std::vector<element<T> *>>>
+    template<typename T, typename C = std::vector<element<T>>>
     class layout {
         C elements;
-        G grid;
 
       public:
         using value_type = T;
