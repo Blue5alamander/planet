cae9fb31b68937a39df544b73c049bb343481b0f
Kirit Sælensminde <kirit@felspar.com>
2023-04-01 17:50:44 +0700

    We're gong to want this header for something else

--- include/planet/ui.hpp
+++ include/planet/ui.hpp
@@ -2,7 +2,10 @@
 
 
 #include <planet/ui/box.hpp>
-#include <planet/ui/layout.hpp>
+#include <planet/ui/layout.column.hpp>
+#include <planet/ui/layout.corners.hpp>
+#include <planet/ui/layout.grid.hpp>
+#include <planet/ui/layout.row.hpp>
 #include <planet/ui/panel.hpp>
 #include <planet/ui/scale.hpp>
 #include <planet/ui/size.hpp>
--- include/planet/ui/layout.hpp
+++ include/planet/ui/layout.hpp
@@ -1,7 +1,1 @@
 #pragma once
-
-
-#include <planet/ui/layout.column.hpp>
-#include <planet/ui/layout.corners.hpp>
-#include <planet/ui/layout.grid.hpp>
-#include <planet/ui/layout.row.hpp>
