487eddb40b8424e2822c99b51add56ff21b99222
Kirit Sælensminde <kirit@felspar.com>
2023-04-08 12:13:03 +0700

    Add a basic camera

---
+++ include/planet/camera/orthogonal_birdseye.hpp
@@ -0,0 +1,46 @@
+#pragma once
+
+
+#include <planet/affine/transform2d.hpp>
+
+#include <felspar/coro/task.hpp>
+#include <felspar/io/warden.hpp>
+
+
+namespace planet::camera {
+
+
+    /// ## Bird's eye view of the XY plane
+    /**
+     * A camera that looks down on the XY plane without any perspective
+     * projection. The location that the camera is looking at can be set using
+     * `target_looking_at` and the zoom level can be set using `target_scale`.
+     * If the `updates` coroutine is used then the camera will animate from the
+     * current position and scale to the targets.
+     */
+    class orthogonal_birdseye {
+      public:
+        planet::affine::transform2d view;
+
+        /// ### Updates the camera view over time to match the targets
+        /**
+         * Should be run in a task manager, e.g.:
+         * ```cpp
+         * felspar::coro::starter<> tasks;
+         * tasks.post(camera, &planet::camera::orthogonal_birdseye::updates,
+         * std::ref(warden));
+         * ```
+         */
+        felspar::coro::task<void> updates(felspar::io::warden &warden);
+
+        /// ### Targets
+        planet::affine::point2d target_looking_at{{}, {}};
+        float target_scale{1};
+
+      private:
+        planet::affine::point2d current_looking_at = target_looking_at;
+        float current_scale = target_scale;
+    };
+
+
+}
---
+++ src/cameras.cpp
@@ -0,0 +1,30 @@
+#include <planet/camera/orthogonal_birdseye.hpp>
+
+
+using namespace std::literals;
+
+
+/// ## `planet::camera::orthogonal_birdseye`
+
+
+felspar::coro::task<void> planet::camera::orthogonal_birdseye::updates(
+        felspar::io::warden &warden) {
+    while (true) {
+        auto const scale_difference = target_scale - current_scale;
+        current_scale += scale_difference * 0.15f;
+
+        auto const direction = target_looking_at - current_looking_at;
+        if (direction.mag2() <= 0.0001f) {
+            current_looking_at = target_looking_at;
+        } else {
+            auto const translate = planet::affine::point2d::from_polar(
+                    std::sqrt(direction.mag2()) * 0.15f, direction.theta());
+            current_looking_at = current_looking_at + translate;
+        }
+
+        view = {};
+        view.translate(-current_looking_at).scale(1.0f / current_scale);
+
+        co_await warden.sleep(25ms);
+    }
+}
