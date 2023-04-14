80a71037ca651eb2c0d6f738bc66efdb93c8a8b9
Kirit Sælensminde <kirit@felspar.com>
2023-04-14 15:26:16 +0700

    Make 3D matrix outputtable to the stream

--- include/planet/ostream.hpp
+++ include/planet/ostream.hpp
@@ -2,6 +2,7 @@
 
 
 #include <planet/affine2d.hpp>
+#include <planet/affine/matrix3d.hpp>
 #include <planet/map/hex.hpp>
 
 #include <ostream>
@@ -19,6 +20,15 @@ namespace planet {
         }
         inline std::ostream &operator<<(std::ostream &os, rectangle2d const r) {
             return os << r.extents << '@' << r.top_left;
+        }
+        inline std::ostream &operator<<(std::ostream &os, matrix3d const &m) {
+            return os << "{{" << m[{0, 0}] << ", " << m[{1, 0}] << ", "
+                      << m[{2, 0}] << ", " << m[{3, 0}] << "}\n {" << m[{0, 1}]
+                      << ", " << m[{1, 1}] << ", " << m[{2, 1}] << ", "
+                      << m[{3, 1}] << "}\n {" << m[{0, 2}] << ", " << m[{1, 2}]
+                      << ", " << m[{2, 2}] << ", " << m[{3, 2}] << "}\n {"
+                      << m[{0, 3}] << ", " << m[{1, 3}] << ", " << m[{2, 3}]
+                      << ", " << m[{3, 3}] << "}}";
         }
     }
 
