073e062912834664a4bb615c14b0db01c895b0ac
Kirit Sælensminde <kirit@felspar.com>
2023-04-24 22:41:33 +0700

    Fetch the first page from the first stream, but leak all the things

--- examples/oggplayer.cpp
+++ examples/oggplayer.cpp
@@ -1,6 +1,11 @@
 #include <planet/asset_manager.hpp>
 
+#include <felspar/memory/hexdump.hpp>
+
+#include <ogg/ogg.h>
+
 #include <iostream>
+#include <map>
 
 
 int main(int const argc, char const *const argv[]) {
@@ -8,6 +13,42 @@ int main(int const argc, char const *const argv[]) {
     if (argc == 2) {
         std::cout << "Loading " << argv[1] << '\n';
         auto const ogg = planet::file_loader::file_data(argv[1]);
+
+        ogg_sync_state oss{};
+        ogg_sync_init(&oss); // Always works
+
+
+        std::byte *const buffer = reinterpret_cast<std::byte *>(
+                ogg_sync_buffer(&oss, ogg.size()));
+        std::copy(ogg.begin(), ogg.end(), buffer);
+        ogg_sync_wrote(&oss, ogg.size());
+
+        std::map<int, ogg_stream_state> streams;
+        ogg_page op;
+        while (ogg_sync_pageout(&oss, &op) == 1) {
+            std::cout << "header: "
+                      << felspar::memory::hexdump(std::span{
+                                 op.header,
+                                 static_cast<std::size_t>(op.header_len)});
+
+            int serialno = ogg_page_serialno(&op);
+            if (not streams.contains(serialno)) {
+                ogg_stream_init(&streams[serialno], serialno);
+            }
+            std::cout << "page in "
+                      << ogg_stream_pagein(&streams[serialno], &op)
+                      << '\n'; // Check for failure
+
+            ogg_packet packet = {};
+            int pread = ogg_stream_packetout(&streams[serialno], &packet);
+            std::cout << "stream: " << serialno << " packet: " << pread << ' '
+                      << felspar::memory::hexdump(std::span{
+                                 packet.packet,
+                                 static_cast<std::size_t>(packet.bytes)});
+
+            break;
+        }
+
         return 0;
     } else {
         std::cerr << "Usage\n\t" << argv[0] << " music.ogg\n";
