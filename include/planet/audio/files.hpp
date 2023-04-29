d5345b90b56c9f4e518c0d46c71037aefb8974dd
Kirit Sælensminde <kirit@felspar.com>
2023-04-29 13:49:53 +0700

    Add ogg file decoding

--- include/planet/audio/files.hpp
+++ include/planet/audio/files.hpp
@@ -10,7 +10,25 @@
 namespace planet::audio {
 
 
-    /// Load a WAV file
+    /// ## Load an ogg file
+    class ogg final {
+        struct impl;
+        std::vector<std::byte> filedata;
+        felspar::source_location loc;
+
+      public:
+        explicit ogg(
+                std::vector<std::byte>,
+                felspar::source_location const & =
+                        felspar::source_location::current());
+
+        felspar::coro::generator<
+                planet::audio::buffer_storage<planet::audio::sample_clock, 2>>
+                stereo();
+    };
+
+
+    /// ## Load a WAV file
     class wav final {
         buffer_storage<sample_clock, 2> samples;
 
--- src/files.audio.cpp
+++ src/files.audio.cpp
@@ -1,5 +1,160 @@
 #include <planet/audio/files.hpp>
 
+#include <vorbis/codec.h>
+
+
+/// ## `planet::audio::ogg::impl`
+
+
+struct planet::audio::ogg::impl {
+    impl(std::span<std::byte>, felspar::source_location const &);
+    ~impl();
+
+    vorbis_info vi;
+    vorbis_comment vc;
+    felspar::source_location loc;
+
+    felspar::coro::generator<
+            planet::audio::buffer_storage<planet::audio::sample_clock, 2>>
+            stereo();
+
+  private:
+    std::span<std::byte> ogg;
+    felspar::coro::generator<ogg_packet> packets;
+
+    felspar::coro::generator<ogg_packet> vorbis_packets();
+};
+
+
+planet::audio::ogg::impl::impl(
+        std::span<std::byte> o, felspar::source_location const &l)
+: loc{l}, ogg{o}, packets{vorbis_packets()} {
+    vorbis_info_init(&vi);
+    vorbis_comment_init(&vc);
+    for (std::size_t expected{3}; expected; --expected) {
+        auto vip = packets.next();
+        if (vorbis_synthesis_headerin(&vi, &vc, &*vip) < 0) {
+            throw felspar::stdexcept::runtime_error{
+                    "Not Vorbis audio data", loc};
+        }
+    }
+}
+
+
+planet::audio::ogg::impl::~impl() {
+    vorbis_comment_clear(&vc);
+    vorbis_info_clear(&vi);
+}
+
+
+felspar::coro::generator<ogg_packet> planet::audio::ogg::impl::vorbis_packets() {
+    ogg_sync_state sync;
+    ogg_stream_state stream;
+    ogg_sync_init(&sync); // Always works
+
+    ogg_page op;
+    if (ogg_sync_pageout(&sync, &op) != 0) {
+        throw felspar::stdexcept::runtime_error{
+                "ogg_sync_pageout not requesting data", loc};
+    }
+
+    std::byte *const buffer =
+            reinterpret_cast<std::byte *>(ogg_sync_buffer(&sync, ogg.size()));
+    std::copy(ogg.begin(), ogg.end(), buffer);
+    if (ogg_sync_wrote(&sync, ogg.size()) == -1) {
+        throw felspar::stdexcept::runtime_error{"ogg_sync_wrote failed", loc};
+    }
+
+    std::optional<int> streamid;
+    while (ogg_sync_pageout(&sync, &op) == 1) {
+        int serialno = ogg_page_serialno(&op);
+        if (not streamid) {
+            streamid = serialno;
+            ogg_stream_init(&stream, serialno);
+        }
+        if (serialno == streamid) {
+            if (ogg_stream_pagein(&stream, &op) == -1) {
+                throw felspar::stdexcept::runtime_error{
+                        "ogg_stream_pagein failed", loc};
+            }
+
+            ogg_packet packet;
+            while (ogg_stream_packetout(&stream, &packet) == 1) {
+                co_yield packet;
+            }
+        }
+    }
+    /// TODO Make exception safe
+    ogg_stream_clear(&stream);
+    ogg_sync_clear(&sync);
+}
+
+
+felspar::coro::generator<
+        planet::audio::buffer_storage<planet::audio::sample_clock, 2>>
+        planet::audio::ogg::impl::stereo() {
+    if (vi.channels != 2) {
+        throw felspar::stdexcept::runtime_error{
+                "This ogg file is not stereo", loc};
+    } else if (vi.rate != planet::audio::sample_clock::period::den) {
+        throw felspar::stdexcept::runtime_error{
+                "The sample rate is wrong. Should be 48kHz and is "
+                        + std::to_string(vi.rate) + "Hz",
+                loc};
+    }
+
+    vorbis_dsp_state vd;
+    vorbis_synthesis_init(&vd, &vi);
+    vorbis_block vb;
+    vorbis_block_init(&vd, &vb);
+
+    for (auto &&packet : packets) {
+        if (vorbis_synthesis(&vb, &packet) == 0) {
+            vorbis_synthesis_blockin(&vd, &vb);
+        } else {
+            throw felspar::stdexcept::runtime_error{
+                    "vorbis_synthesis failed", loc};
+        }
+        float **pcm = nullptr;
+        while (int samples = vorbis_synthesis_pcmout(&vd, &pcm)) {
+            planet::audio::buffer_storage<planet::audio::sample_clock, 2> buffer{
+                    static_cast<std::size_t>(samples)};
+            for (std::size_t sample{}; sample < buffer.samples(); ++sample) {
+                buffer[sample][0] = pcm[0][sample];
+                buffer[sample][1] = pcm[1][sample];
+            }
+            vorbis_synthesis_read(&vd, samples);
+            co_yield std::move(buffer);
+        }
+    }
+
+    vorbis_block_clear(&vb);
+    vorbis_dsp_clear(&vd);
+
+    co_return;
+}
+
+
+/// ## `planet::audio::ogg`
+
+
+planet::audio::ogg::ogg(
+        std::vector<std::byte> o, felspar::source_location const &l)
+: filedata{std::move(o)}, loc{l} {}
+
+
+felspar::coro::generator<
+        planet::audio::buffer_storage<planet::audio::sample_clock, 2>>
+        planet::audio::ogg::stereo() {
+    impl decoder{filedata, loc};
+    for (auto s = decoder.stereo(); auto p = s.next();) {
+        co_yield std::move(*p);
+    }
+}
+
+
+/// ## `planet::audio::wav`
+
 
 planet::audio::wav::wav(std::span<std::byte const> filedata)
 : samples{[&]() -> buffer_storage<sample_clock, 2> {
