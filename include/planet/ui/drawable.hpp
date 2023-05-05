821b14ef0f74ae2ce2ee565f762eeda5b7456603
Kirit Sælensminde <kirit@felspar.com>
2023-05-05 11:26:58 +0700

    New drawable interface

--- include/planet/ui/drawable.hpp
+++ include/planet/ui/drawable.hpp
@@ -8,10 +8,16 @@ namespace planet::ui {
     template<typename Renderer>
     struct drawable {
         /// ### Draw at the calculated position
-        void draw_at(Renderer &, affine::point2d const &offset) const;
-
-      protected:
-        // virtual void draw(Renderer &, affine::point2d const &offset) const = 0;
+        /**
+         * On the face of it, this should be `const`, but some UI elements need
+         * to upload data to the GPU and the only time they get a `Renderer` to
+         * do that with is in the draw call.
+         *
+         * This probably means that there's a missing initialisation phase that
+         * is able to make use of the `Renderer` instance, but for our simple
+         * needs right now, this should suffice.
+         */
+        virtual void draw(Renderer &) = 0;
     };
 
 
