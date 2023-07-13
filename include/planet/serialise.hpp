aa6b3039c8cddb392db01cd3c6cf309c215bd559
Kirit Sælensminde <kirit@felspar.com>
2023-07-13 18:52:24 +0700

    Add missing header and re-order

--- include/planet/serialise.hpp
+++ include/planet/serialise.hpp
@@ -5,11 +5,12 @@
 #include <planet/serialise/base_types.hpp>
 #include <planet/serialise/chrono.hpp>
 #include <planet/serialise/events.hpp>
+#include <planet/serialise/exceptions.hpp>
 #include <planet/serialise/felspar.hpp>
 #include <planet/serialise/forward.hpp>
-#include <planet/serialise/save_buffer.hpp>
 #include <planet/serialise/load_buffer.hpp>
 #include <planet/serialise/map.hpp>
 #include <planet/serialise/marker.hpp>
 #include <planet/serialise/muxing.hpp>
+#include <planet/serialise/save_buffer.hpp>
 #include <planet/serialise/string.hpp>
