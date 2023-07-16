7488f77cf0c6fbd915689deeb0c2b07a7e1f8a82
Kirit Sælensminde <kirit@felspar.com>
2023-07-16 14:20:08 +0700

    Add some tests to make sure that the vector column layout is right

--- test/column.layout.tests.cpp
+++ test/column.layout.tests.cpp
@@ -85,4 +85,40 @@ namespace {
             });
 
 
+    auto const vrf1 = suite.test("reflow/vector", [](auto check) {
+        planet::ui::constrained2d<float> const constraints{
+                {100, 0, 100}, {100, 0, 100}};
+        planet::affine::rectangle2d const size{
+                {13, 15}, planet::affine::extents2d{100, 100}};
+        auto c =
+                planet::ui::column{std::vector<planet::debug::fixed_element>{}};
+        check(c.items.size()) == 0u;
+
+        c.reflow(constraints);
+        c.move_to(size);
+        check(c.position().extents) == planet::affine::extents2d{};
+
+        c.items.push_back({{4, 3}});
+        c.reflow(constraints);
+        c.move_to(size);
+        check(c.position().top_left) == planet::affine::point2d{13, 15};
+        check(c.position().extents) == planet::affine::extents2d{4, 3};
+        check(c.items[0].position().top_left)
+                == planet::affine::point2d{13, 15};
+        check(c.items[0].position().extents) == planet::affine::extents2d{4, 3};
+
+        c.items.push_back({{4, 3}});
+        c.reflow(constraints);
+        c.move_to(size);
+        check(c.position().top_left) == planet::affine::point2d{13, 15};
+        check(c.position().extents) == planet::affine::extents2d{4, 6};
+        check(c.items[0].position().top_left)
+                == planet::affine::point2d{13, 15};
+        check(c.items[0].position().extents) == planet::affine::extents2d{4, 3};
+        check(c.items[1].position().top_left)
+                == planet::affine::point2d{13, 18};
+        check(c.items[0].position().extents) == planet::affine::extents2d{4, 3};
+    });
+
+
 }
