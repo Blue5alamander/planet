64d1239c6e991711892f30419407d51b235d4ef3
Kirit Sælensminde <kirit@felspar.com>
2023-10-29 20:09:43 +0700

    Also support buttons that can set a value on a future

--- include/planet/ui/button.hpp
+++ include/planet/ui/button.hpp
@@ -4,46 +4,56 @@
 #include <planet/queue/pmc.hpp>
 #include <planet/ui/widget.hpp>
 
+#include <felspar/coro/future.hpp>
+
 
 namespace planet::ui {
 
 
+    /// ## Button
+    /**
+     * A UI element that sets a particular value when a mouse click is detected
+     * inside its area. The button is customised with a graphical element. The
+     * value of the mouse press is always the same, and is passed to the output.
+     */
     template<
             typename Renderer,
             typename Return,
             typename Texture,
-            typename Queue = queue::pmc<Return>>
+            typename Output = queue::pmc<Return>>
     class button : public planet::ui::widget<Renderer> {
         using superclass = planet::ui::widget<Renderer>;
         Return press_value;
-        Queue &output_to;
+        Output &output_to;
 
 
       public:
-        using queue_type = Queue;
+        using value_type = Return;
+        using output_type = Output;
         using constrained_type = typename superclass::constrained_type;
         using superclass::name;
         using superclass::position;
 
 
-        explicit button(queue_type &o, Return v)
+        explicit button(output_type &o, value_type v)
         : superclass{"planet::sdl::ui::button"},
           press_value{std::move(v)},
           output_to{o} {}
-        button(std::string_view const n, queue_type &o, Return v)
+        button(std::string_view const n, output_type &o, value_type v)
         : superclass{n}, press_value{std::move(v)}, output_to{o} {}
-        button(Texture g, queue_type &o, Return v)
+        button(Texture g, output_type &o, value_type v)
         : superclass{"planet::sdl::ui::button"},
           press_value{std::move(v)},
           output_to{o},
           graphic{std::move(g)} {}
-        button(std::string_view const n, Texture g, queue_type &o, Return v)
+        button(std::string_view const n, Texture g, output_type &o, value_type v)
         : superclass{n},
           press_value{std::move(v)},
           output_to{o},
           graphic{std::move(g)} {}
 
         Texture graphic;
+
 
       private:
         constrained_type do_reflow(constrained_type const &ex) override {
@@ -53,13 +63,30 @@ namespace planet::ui {
             graphic.move_to(r);
         }
 
-        felspar::coro::task<void> behaviour() override {
-            for (auto clicks = events::identify_clicks(
-                         superclass::baseplate->mouse_settings,
-                         superclass::events.mouse.stream());
-                 auto click = co_await clicks.next();) {
-                output_to.push(press_value);
+        template<typename Q>
+        struct handle;
+        template<typename R>
+        struct handle<queue::pmc<R>> {
+            static felspar::coro::task<void> press(button *self, auto clicks) {
+                while (true) {
+                    auto click = co_await clicks.next();
+                    self->output_to.push(self->press_value);
+                }
             }
+        };
+        template<typename R>
+        struct handle<felspar::coro::future<R>> {
+            static felspar::coro::task<void> press(button *self, auto clicks) {
+                co_await clicks.next();
+                self->output_to.set_value(self->press_value);
+            }
+        };
+        felspar::coro::task<void> behaviour() override {
+            return handle<output_type>::press(
+                    this,
+                    events::identify_clicks(
+                            superclass::baseplate->mouse_settings,
+                            superclass::events.mouse.stream()));
         }
     };
 
