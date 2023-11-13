3ea2f8763955e582202b16a151fe4ff790fb6109
Kirit Sælensminde <kirit@felspar.com>
2023-11-13 19:23:45 +0700

    Add a single consumer queue

---
+++ include/planet/queue/psc.hpp
@@ -0,0 +1,74 @@
+#pragma once
+
+
+#include <felspar/coro/coroutine.hpp>
+
+#include <vector>
+
+
+namespace planet::queue {
+
+
+    /// ## Push producer, single consumer queue
+    /**
+     * A queue connects multiple producers with a single consumer. The consumer
+     * must be a coroutine, but the producers may be normal functions. If no
+     * coroutine is currently waiting then pushed values are held until one
+     * wants a value.
+     *
+     * The type may be move only.
+     *
+     * Like the bus, it is important that the consumption not kill the producer
+     * that just pushed a value.
+     */
+    template<typename T>
+    class psc final {
+        std::vector<T> values;
+        felspar::coro::coroutine_handle<> waiting = {};
+
+      public:
+        psc() = default;
+
+
+        bool empty() const noexcept { return values.empty(); }
+
+
+        auto next() {
+            struct awaitable {
+                psc &q;
+
+                bool await_ready() const noexcept {
+                    return not q.values.empty();
+                }
+                void await_suspend(felspar::coro::coroutine_handle<> h) {
+                    if (q.waiting) {
+                        throw felspar::stdexcept::logic_error{
+                                "There is already a coroutine waiting on this "
+                                "queue"};
+                    } else {
+                        q.waiting = h;
+                    }
+                }
+                T await_resume() {
+                    T ret = std::move(q.values.front());
+                    q.values.erase(q.values.begin());
+                    return ret;
+                }
+            };
+            return awaitable{*this};
+        }
+
+
+        bool push(T t) {
+            values.push_back(std::move(t));
+            if (waiting) {
+                std::exchange(waiting, {}).resume();
+                return true;
+            } else {
+                return false;
+            }
+        }
+    };
+
+
+}
