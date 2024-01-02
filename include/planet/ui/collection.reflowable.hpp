a99da47a0af0ccec52394fd5fe9e7a24518a9ba3
Kirit Sælensminde <kirit@felspar.com>
2024-01-02 13:28:28 +0700

    Collection based reflowables now also honour visibility checks

--- include/planet/ui/collection.reflowable.hpp
+++ include/planet/ui/collection.reflowable.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 
+#include <planet/ui/concepts.hpp>
 #include <planet/ui/layout.hpp>
 #include <planet/ui/reflowable.hpp>
 
@@ -16,8 +17,11 @@ namespace planet::ui {
     template<typename CT, typename ET>
     struct collection_reflowable : public reflowable {
         using collection_type = CT;
+        using content_type = typename CT::value_type;
+
         using layout_type = planet::ui::layout_for<collection_type>;
         using constrained_type = typename layout_type::constrained_type;
+        using element_type = typename layout_type::element_type;
 
 
         explicit collection_reflowable(std::string_view const n)
@@ -33,6 +37,21 @@ namespace planet::ui {
 
         void draw() {
             for (auto &item : items) { item.draw(); }
+        }
+
+
+        /// ### Visibility
+        void visible(bool const v)
+            requires(visibility<content_type>)
+        {
+            for (auto &i : items) { i.visible(v); }
+        }
+        bool is_visible() const noexcept
+            requires(visibility<content_type>)
+        {
+            bool v = false;
+            for (auto const &i : items) { v = v or i.is_visible(); }
+            return v;
         }
 
 
