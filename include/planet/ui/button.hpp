71d1954d318772daaffa3758fb88ed45faf0ff51
Kirit Sælensminde <kirit@felspar.com>
2024-01-05 16:48:35 +0700

    Buttons over void now also support lambda callbacks

--- include/planet/ui/button.hpp
+++ include/planet/ui/button.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 
+#include <planet/queue/concepts.hpp>
 #include <planet/queue/pmc.hpp>
 #include <planet/ui/drawable.hpp>
 #include <planet/ui/widget.hpp>
@@ -18,7 +19,7 @@ namespace planet::ui {
      * value of the mouse press is always the same, and is passed to the output.
      */
     template<typename Return, drawable Texture, typename Output>
-    class button : public widget {
+    class button final : public widget {
         Return press_value;
         Output &output_to;
 
@@ -50,6 +51,15 @@ namespace planet::ui {
           press_value{std::move(v)},
           output_to{o},
           graphic{std::move(g)} {}
+
+        button(button &&b)
+        : widget{std::move(b)},
+          press_value{std::move(b.press_value)},
+          output_to{b.output_to},
+          graphic{std::move(b.graphic)} {
+            if (baseplate) { response.post(behaviour()); }
+        }
+
 
         Texture graphic;
 
@@ -83,6 +93,7 @@ namespace planet::ui {
 
 
       protected:
+        void do_draw() override { graphic.draw(); }
         constrained_type do_reflow(constrained_type const &ex) override {
             return graphic.reflow(ex);
         }
@@ -102,13 +113,49 @@ namespace planet::ui {
     };
 
     template<drawable Texture, typename Output>
-    class button<void, Texture, Output> : public widget {
-        Output &output_to;
+    class button<void, Texture, Output> final : public widget {
+        template<typename Q>
+        struct handle;
+
+        template<queue::push_void_queue Q>
+        struct handle<Q> {
+            static constexpr bool reference = true;
+            using storage = Q &;
+            static felspar::coro::task<void> press(button *self, auto clicks) {
+                while (true) {
+                    auto click = co_await clicks.next();
+                    self->output_to.push();
+                }
+            }
+        };
+        template<typename R>
+        struct handle<felspar::coro::future<R>> {
+            static constexpr bool reference = true;
+            using storage = felspar::coro::future<R> &;
+            static felspar::coro::task<void> press(button *self, auto clicks) {
+                co_await clicks.next();
+                self->output_to.set_value();
+            }
+        };
+        template<std::invocable<> Q>
+        struct handle<Q> {
+            static constexpr bool reference = false;
+            using storage = Q;
+            static felspar::coro::task<void> press(button *self, auto clicks) {
+                while (true) {
+                    auto click = co_await clicks.next();
+                    self->output_to();
+                }
+            }
+        };
+
+
+        typename handle<std::decay_t<Output>>::storage output_to;
 
 
       public:
         using value_type = void;
-        using output_type = Output;
+        using output_type = std::decay_t<Output>;
         using constrained_type = widget::constrained_type;
         using widget::name;
         using widget::position;
@@ -124,30 +171,30 @@ namespace planet::ui {
           graphic{std::move(g)} {}
         button(std::string_view const n, Texture g, output_type &o)
         : widget{n}, output_to{o}, graphic{std::move(g)} {}
+        button(std::string_view const n, Texture g, output_type &&o)
+        : widget{n}, output_to{std::move(o)}, graphic{std::move(g)} {}
+
+        button(button &&b)
+            requires handle<output_type>::reference
+        : widget{std::move(b)},
+          output_to{b.output_to},
+          graphic{std::move(b.graphic)} {
+            if (baseplate) { response.post(behaviour()); }
+        }
+        button(button &&b)
+            requires(not handle<output_type>::reference)
+        : widget{std::move(b)},
+          output_to{std::move(b.output_to)},
+          graphic{std::move(b.graphic)} {
+            if (baseplate) { response.post(behaviour()); }
+        }
+
 
         Texture graphic;
 
 
-      private:
-        template<typename Q>
-        struct handle {
-            static felspar::coro::task<void> press(button *self, auto clicks) {
-                while (true) {
-                    auto click = co_await clicks.next();
-                    self->output_to.push();
-                }
-            }
-        };
-        template<typename R>
-        struct handle<felspar::coro::future<R>> {
-            static felspar::coro::task<void> press(button *self, auto clicks) {
-                co_await clicks.next();
-                self->output_to.set_value();
-            }
-        };
-
-
       protected:
+        void do_draw() override { graphic.draw(); }
         constrained_type do_reflow(constrained_type const &ex) override {
             return graphic.reflow(ex);
         }
@@ -165,6 +212,10 @@ namespace planet::ui {
                             widget::events.mouse.stream()));
         }
     };
+
+
+    template<drawable G, typename Q>
+    button(std::string_view, G, Q) -> button<void, G, Q>;
 
 
 }
