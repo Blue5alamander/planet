7728114e4ac77a95e725699ce5501442ccd3cbc9
Kirit Sælensminde <kirit@felspar.com>
2024-01-05 16:47:44 +0700

    Add basic concepts describing push queues

---
+++ include/planet/queue/concepts.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+
+namespace planet::queue {
+
+
+    template<typename Q>
+    concept push_value_queue = requires(Q q) {
+        typename Q::value_type;
+        { q.push(std::declval<Q::value_type>()) };
+    };
+    template<typename Q>
+    concept push_void_queue = requires(Q q) {
+        { q.push() };
+    };
+
+
+}
