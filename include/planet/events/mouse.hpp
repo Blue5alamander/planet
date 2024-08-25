da9cdb1989e239328b6964eeafd5b4a0eb53532f
Kirit Sælensminde <kirit@felspar.com>
2024-08-25 17:02:10 +0700

    Add note to say we need to change the click identification

--- include/planet/events/mouse.hpp
+++ include/planet/events/mouse.hpp
@@ -41,6 +41,10 @@ namespace planet::events {
     /// ## Mouse event processing
 
     /// ### Process a stream of mouse data into higher level events
+    /**
+     * TODO This probably needs to go. SDL already provides the mouse click
+     * number and we should always just use that.
+     */
     felspar::coro::stream<click> identify_clicks(felspar::coro::stream<mouse>);
 
 
