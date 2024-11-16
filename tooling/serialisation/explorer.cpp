de7f95f3c1706a482a0890248e392079fbe1921c
Kirit Sælensminde <kirit@felspar.com>
2024-11-16 10:34:47 +0700

    Fix output for maps and improve optionals

--- tooling/serialisation/explorer.cpp
+++ tooling/serialisation/explorer.cpp
@@ -1,59 +1,81 @@
 #include <planet/serialise.hpp>
 
-#include <felspar/parse.binary.hpp>
-
 #include <filesystem>
 #include <fstream>
 #include <iostream>
 
 
-using namespace felspar::parse::binary;
-using namespace planet;
-
-
 namespace {
-    void show(serialise::load_buffer &lb, std::size_t const depth) {
+    void show(planet::serialise::load_buffer &, std::size_t);
+    void show(planet::serialise::box &box, std::size_t const depth) {
+        std::cout << box.name << " v" << int(box.version) << " size "
+                  << box.content.size() << " bytes";
+        if (box.name == "_s:opt") {
+            auto const has_value = load_type<bool>(box.content);
+            if (has_value) {
+                std::cout << " (with value)\n";
+                show(box.content, depth + 1);
+            } else if (not box.content.empty()) {
+                std::cout << " (shows as empty, but has content)\n";
+                show(box.content, depth + 1);
+            } else {
+                std::cout << " (empty)\n";
+            }
+        } else if (box.name == "_s:map") {
+            auto const entries = box.content.extract_size_t();
+            std::cout << " with " << entries << " entries\n";
+            show(box.content, depth + 1);
+        } else {
+            std::cout << '\n';
+            show(box.content, depth + 1);
+        }
+    }
+    void show(planet::serialise::load_buffer &lb, std::size_t const depth) {
         while (not lb.empty()) {
             std::cout << std::string(depth, ' ');
 
             auto const mv = static_cast<std::uint8_t>(lb.cmemory()[0]);
             if (mv > 0 and mv < 80) {
-                auto b = load_type<serialise::box>(lb);
-                std::cout << b.name << " v" << int(b.version) << " size "
-                          << b.content.size() << " bytes\n";
-                show(b.content, depth + 1);
+                auto box = load_type<planet::serialise::box>(lb);
+                show(box, depth);
             } else {
                 auto const m = lb.extract_marker();
                 switch (m) {
-                case serialise::marker::empty: std::cout << "empty\n"; break;
+                case planet::serialise::marker::empty:
+                    std::cout << "empty\n";
+                    break;
 
-                case serialise::marker::b_true: std::cout << "true\n"; break;
-                case serialise::marker::b_false: std::cout << "false\n"; break;
+                case planet::serialise::marker::b_true:
+                    std::cout << "true\n";
+                    break;
+                case planet::serialise::marker::b_false:
+                    std::cout << "false\n";
+                    break;
 
-                case serialise::marker::u8:
+                case planet::serialise::marker::u8:
                     std::cout
                             << static_cast<unsigned>(lb.extract<std::uint8_t>())
                             << ' ' << to_string(m) << '\n';
                     break;
-                case serialise::marker::i32le:
+                case planet::serialise::marker::i32le:
                     std::cout << lb.extract<std::int32_t>() << ' '
                               << to_string(m) << '\n';
                     break;
-                case serialise::marker::u64le:
+                case planet::serialise::marker::u64le:
                     std::cout << lb.extract<std::uint64_t>() << ' '
                               << to_string(m) << '\n';
                     break;
-                case serialise::marker::i64le:
+                case planet::serialise::marker::i64le:
                     std::cout << lb.extract<std::int64_t>() << ' '
                               << to_string(m) << '\n';
                     break;
 
-                case serialise::marker::f128le:
+                case planet::serialise::marker::f128le:
                     std::cout << lb.extract<long double>() << ' '
                               << to_string(m) << '\n';
                     break;
 
-                case serialise::marker::poly_list: {
+                case planet::serialise::marker::poly_list: {
                     auto const count = lb.extract_size_t();
                     std::cout << "poly-list with " << count << " items\n";
                     for (std::size_t index{}; index < count; ++index) {
@@ -62,7 +84,7 @@ namespace {
                     break;
                 }
 
-                case serialise::marker::u8string8: {
+                case planet::serialise::marker::u8string8: {
                     std::string str;
                     auto const sz = lb.extract_size_t();
                     auto const b = lb.split(sz);
@@ -95,7 +117,7 @@ int main(int argc, char const *argv[]) {
     std::vector<char> save(std::filesystem::file_size(fn), std::ios::binary);
     input.read(save.data(), save.size());
 
-    serialise::load_buffer buffer{
+    planet::serialise::load_buffer buffer{
             std::as_bytes(std::span{save.data(), save.size()})};
     show(buffer, 0);
 
