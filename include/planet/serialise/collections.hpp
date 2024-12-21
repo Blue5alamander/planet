f1f8317096c22586637f1da2bb21dfa945f0e7a1
Kirit Sælensminde <kirit@felspar.com>
2024-12-21 13:30:02 +0700

    Save `std::map` so that the explorer can interpret them

--- include/planet/serialise/collections.hpp
+++ include/planet/serialise/collections.hpp
@@ -9,8 +9,8 @@ namespace planet::serialise {
 
     template<typename K, typename V, typename C, typename A>
     void save(save_buffer &ab, std::map<K, V, C, A> const &m) {
-        ab.save_box_lambda("_s:map", [&]() {
-            ab.append_size_t(m.size());
+        ab.save_box_lambda(2, "_s:map", [&]() {
+            save(ab, m.size());
             for (auto const &[k, v] : m) {
                 save(ab, k);
                 save(ab, v);
@@ -22,9 +22,19 @@ namespace planet::serialise {
         m.clear();
         auto box = load_type<serialise::box>(lb);
         box.check_name_or_throw("_s:map");
-        auto const count = box.content.extract_size_t();
-        while (m.size() < count) {
-            load(box.content, m[load_type<K>(box.content)]);
+        if (box.version == 2) {
+            std::size_t count;
+            load(box.content, count);
+            while (m.size() < count) {
+                load(box.content, m[load_type<K>(box.content)]);
+            }
+        } else if (box.version == 1) {
+            auto const count = box.content.extract_size_t();
+            while (m.size() < count) {
+                load(box.content, m[load_type<K>(box.content)]);
+            }
+        } else {
+            box.throw_unsupported_version(2);
         }
         box.check_empty_or_throw();
     }
