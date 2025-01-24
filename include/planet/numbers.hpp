b226bf8ad82a823dd529ef8fb45b75a8b1addc90
Kirit Sælensminde <kirit@felspar.com>
2025-01-24 15:38:28 +0700

    Add value for pi

--- include/planet/numbers.hpp
+++ include/planet/numbers.hpp
@@ -5,6 +5,7 @@
 #include <numbers>
 namespace planet {
     /// Constant for radian conversions
+    constexpr float pi = std::numbers::pi_v<float>;
     constexpr float tau = std::numbers::pi_v<float> * 2.0f;
     constexpr float sqrt3 = std::numbers::sqrt3_v<float>;
 }
@@ -12,6 +13,7 @@ namespace planet {
 namespace planet {
     /// Constant for radian conversions
     constexpr float tau = 6.283185307179586f;
+    constexpr float pi = tau / 2.0f;
     constexpr float sqrt3 = 1.7320508075688772f;
 }
 #endif
