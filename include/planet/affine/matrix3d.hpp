6a9b7f29659e1d22dceb77b5c9c62ac8d29c4be0
Kirit Sælensminde <kirit@felspar.com>
2025-02-20 23:24:34 +0700

    Add new translation and scaling APIs

--- include/planet/affine/matrix3d.hpp
+++ include/planet/affine/matrix3d.hpp
@@ -52,11 +52,22 @@ namespace planet::affine {
                      std::array{0.0f, 0.0f, 1.0f, z},
                      std::array{0.0f, 0.0f, 0.0f, 1.0f}}};
         }
+        static constexpr matrix3d translate(point3d const &p) {
+            return translate(p.x(), p.y(), p.z());
+        }
         static constexpr matrix3d scale_xy(float const x, float const y) {
             return {
                     {std::array{x, 0.0f, 0.0f, 0.0f},
                      std::array{0.0f, y, 0.0f, 0.0f},
                      std::array{0.0f, 0.0f, 1.0f, 0.0f},
+                     std::array{0.0f, 0.0f, 0.0f, 1.0f}}};
+        }
+        static constexpr matrix3d
+                scale_xyz(float const x, float const y, float const z) {
+            return {
+                    {std::array{x, 0.0f, 0.0f, 0.0f},
+                     std::array{0.0f, y, 0.0f, 0.0f},
+                     std::array{0.0f, 0.0f, z, 0.0f},
                      std::array{0.0f, 0.0f, 0.0f, 1.0f}}};
         }
         static constexpr matrix3d rotate_x(float const t) {
