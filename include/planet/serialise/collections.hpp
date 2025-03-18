88a78211f7ba27c66214bc5cb8b10fc41e2734fb
Kirit Sælensminde <kirit@felspar.com>
2025-03-18 10:00:53 +0700

    Add serialisation for `std::set`

--- include/planet/serialise/collections.hpp
+++ include/planet/serialise/collections.hpp
@@ -2,11 +2,13 @@
 
 
 #include <map>
+#include <set>
 
 
 namespace planet::serialise {
 
 
+    /// ## Serialise `std::map`
     template<typename K, typename V, typename C, typename A>
     void save(save_buffer &ab, std::map<K, V, C, A> const &m) {
         ab.save_box_lambda(2, "_s:map", [&]() {
@@ -34,6 +36,25 @@ namespace planet::serialise {
         while (m.size() < count) {
             load(box.content, m[load_type<K>(box.content)]);
         }
+        box.check_empty_or_throw();
+    }
+
+
+    /// ## Serialise `std::set`
+    template<typename K, typename C, typename A>
+    void save(save_buffer &ab, std::set<K, C, A> const &m) {
+        ab.save_box_lambda(1, "_s:set", [&]() {
+            save(ab, m.size());
+            for (auto const &k : m) { save(ab, k); }
+        });
+    }
+    template<typename K, typename C, typename A>
+    void load(load_buffer &lb, std::set<K, C, A> &m) {
+        m.clear();
+        auto box = expect_box(lb);
+        box.check_name_or_throw("_s:set");
+        auto const count = load_type<std::size_t>(box.content);
+        while (m.size() < count) { m.insert(load_type<K>(box.content)); }
         box.check_empty_or_throw();
     }
 
