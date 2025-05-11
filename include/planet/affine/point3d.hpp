fd7cf1f1383dfd88c0cbf0b42314a88f223375bd
Kirit Sælensminde <kirit@felspar.com>
2025-05-11 14:18:06 +0700

    Extend the point3d API with more useful functionality

--- include/planet/affine/point3d.hpp
+++ include/planet/affine/point3d.hpp
@@ -31,14 +31,27 @@ namespace planet::affine {
         constexpr float z() const noexcept { return zh / h; }
         constexpr float z(float const n) noexcept { return zh = n * h; }
 
+        constexpr point2d xy() const noexcept { return {x(), y()}; }
+
+        /// #### Square of the distance from origin
         constexpr float mag2() const noexcept {
             return x() * x() + y() * y() + z() * z();
         }
 
+        /// #### Dot product
+        constexpr float dot(point3d const &p) const noexcept {
+            return x() * p.x() + y() * p.y() + z() * p.z();
+        }
+
 
         /// ### Operators
+
+        /// #### Maths operations
         friend constexpr point3d operator-(point3d const &p) {
             return {p.xh, p.yh, p.zh, -p.h};
+        }
+        friend constexpr point3d operator-(point3d const &l, point3d const &r) {
+            return {l.x() - r.x(), l.y() - r.y(), l.z() - r.z()};
         }
 
         point3d &operator+=(point3d const &r) { return *this = *this + r; }
@@ -54,10 +67,17 @@ namespace planet::affine {
             zh /= s;
             return *this;
         }
+        friend point3d operator/(point3d const &p, float const s) {
+            return {p.xh / s, p.yh / s, p.zh / s, p.h};
+        }
 
         friend constexpr point3d operator*(point3d const &l, float const r) {
             return {l.xh * r, l.yh * r, l.zh * r, l.h};
         }
+
+        /// #### Unit vector
+        /// TODO Make constexpr when g++ catches up
+        point3d normalise() const noexcept { return *this / std::sqrt(mag2()); }
     };
 
 
