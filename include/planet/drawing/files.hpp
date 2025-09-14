e4da208279eafd56b7f4cb6f88dd2675e03a309c
Kirit Sælensminde <kirit@felspar.com>
2025-09-14 12:57:08 +0700

    Add red/green/blue/alpha accessors

--- include/planet/drawing/files.hpp
+++ include/planet/drawing/files.hpp
@@ -36,17 +36,36 @@ namespace planet::drawing {
         std::size_t header_size() const noexcept;
         std::size_t pixels_size() const;
 
+
       public:
         bmp(std::size_t width, std::size_t height);
 
+
         std::vector<std::vector<std::uint8_t>> pixels;
+
 
         /// ### Sizes
         std::size_t width() const;
         std::size_t height() const;
 
+
         /// ### Save the file
         void save(std::filesystem::path const &) const;
+
+
+        /// ### Colour accessors
+        std::uint8_t &red(std::size_t const x, std::size_t const y) {
+            return pixels.at(pixels.size() - y - 1).at(x * 4 + 2);
+        }
+        std::uint8_t &green(std::size_t const x, std::size_t const y) {
+            return pixels.at(pixels.size() - y - 1).at(x * 4 + 1);
+        }
+        std::uint8_t &blue(std::size_t const x, std::size_t const y) {
+            return pixels.at(pixels.size() - y - 1).at(x * 4 + 0);
+        }
+        std::uint8_t &alpha(std::size_t const x, std::size_t const y) {
+            return pixels.at(pixels.size() - y - 1).at(x * 4 + 3);
+        }
     };
 
 
