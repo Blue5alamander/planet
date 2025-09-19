556a7a9a47b06b25618fe722532ce68aeb1c791d
Kirit Sælensminde <kirit@felspar.com>
2025-09-19 16:08:32 +0700

    Make `constexpr` and add `std::array const` overload

--- include/planet/functional.hpp
+++ include/planet/functional.hpp
@@ -29,20 +29,27 @@ namespace planet {
 
     /// ### Nullary lambda
     template<std::invocable<> Lambda>
-    inline auto by_index(std::size_t const max_index, Lambda &&v) {
+    constexpr inline auto by_index(std::size_t const max_index, Lambda &&v) {
         return by_index({}, max_index, [&](std::size_t) { return v(); });
     }
 
 
     /// ### Over a `std::span`
     template<typename T, std::size_t N, std::invocable<std::size_t, T &> Lambda>
-    inline auto by_index(std::span<T, N> s, Lambda &&v) {
+    constexpr inline auto by_index(std::span<T, N> s, Lambda &&v) {
         for (std::size_t index{}; index < s.size(); ++index) {
             v(index, s[index]);
         }
     }
     template<typename T, std::size_t N, std::invocable<std::size_t, T &> Lambda>
-    inline auto by_index(std::array<T, N> &a, Lambda &&v) {
+    constexpr inline auto by_index(std::array<T, N> &a, Lambda &&v) {
+        by_index(std::span{a}, std::forward<Lambda>(v));
+    }
+    template<
+            typename T,
+            std::size_t N,
+            std::regular_invocable<std::size_t, T &> Lambda>
+    constexpr inline auto by_index(std::array<T, N> const &a, Lambda &&v) {
         by_index(std::span{a}, std::forward<Lambda>(v));
     }
 
