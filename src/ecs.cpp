e507963980256e9b5d3ce511231d75f0b3fb2699
Kirit Sælensminde <kirit@felspar.com>
2025-11-30 15:27:07 +0700

    More cleanly separate the names

--- src/ecs.cpp
+++ src/ecs.cpp
@@ -72,9 +72,9 @@ void planet::ecs::detail::throw_wrong_generation(
 
 
 namespace {
-    planet::telemetry::counter create_count{"planet_ecs_entities_created"};
-    planet::telemetry::counter recycle_count{"planet_ecs_entities_recycled"};
-    planet::telemetry::counter destroy_count{"planet_ecs_entities_destroyed"};
+    planet::telemetry::counter create_count{"planet__ecs__entities_created"};
+    planet::telemetry::counter recycle_count{"planet__ecs__entities_recycled"};
+    planet::telemetry::counter destroy_count{"planet__ecs__entities_destroyed"};
 }
 void planet::ecs::detail::count_create_entity() noexcept { ++create_count; }
 void planet::ecs::detail::count_recycled_entity() noexcept { ++recycle_count; }
