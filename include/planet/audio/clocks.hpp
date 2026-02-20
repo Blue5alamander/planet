9a4a61295813f77e97af8f3d9113ae750a12efa0
Kirit Sælensminde <kirit@felspar.com>
2026-02-20 12:48:02 +0700

    Add `samples_per_second` as a constant

--- include/planet/audio/clocks.hpp
+++ include/planet/audio/clocks.hpp
@@ -10,14 +10,16 @@ namespace planet::audio {
     using namespace std::literals;
 
 
-    /// Standard audio output sample rate
+    /// ## Standard audio output sample rate
     using sample_clock =
             std::chrono::duration<std::int64_t, std::ratio<1, 48'000>>;
 
+    std::size_t constexpr samples_per_second = sample_clock::period::den;
 
-    /// The amount of time for a buffer slice
-    constexpr sample_clock default_buffer_duration = 20ms;
-    constexpr std::size_t default_buffer_samples =
+
+    /// ## The amount of time for a standard buffer slice
+    sample_clock constexpr default_buffer_duration = 20ms;
+    std::size_t constexpr default_buffer_samples =
             default_buffer_duration.count();
 
 
