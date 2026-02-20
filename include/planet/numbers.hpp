00f63b42943f20d4844e02ab09cf7db7174f4e86
Kirit Sælensminde <kirit@felspar.com>
2026-02-20 15:43:25 +0700

    Add a constant for `log10`

--- include/planet/numbers.hpp
+++ include/planet/numbers.hpp
@@ -5,15 +5,20 @@
 #include <numbers>
 namespace planet {
     /// Constant for radian conversions
-    constexpr float pi = std::numbers::pi_v<float>;
-    constexpr float tau = std::numbers::pi_v<float> * 2.0f;
-    constexpr float sqrt3 = std::numbers::sqrt3_v<float>;
+    float constexpr pi = std::numbers::pi_v<float>;
+    float constexpr tau = std::numbers::pi_v<float> * 2.0f;
+    float constexpr sqrt3 = std::numbers::sqrt3_v<float>;
 }
 #else
 namespace planet {
     /// Constant for radian conversions
-    constexpr float tau = 6.283185307179586f;
-    constexpr float pi = tau / 2.0f;
-    constexpr float sqrt3 = 1.7320508075688772f;
+    float constexpr tau = 6.283185307179586f;
+    float constexpr pi = tau / 2.0f;
+    float constexpr sqrt3 = 1.7320508075688772f;
 }
 #endif
+
+
+namespace planet {
+    float constexpr log10 = 2.302585092994046f;
+}
