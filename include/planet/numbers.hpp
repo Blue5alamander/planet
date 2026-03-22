67af2e02ab141d08d3e0c2809e7a427fc004d567
Kirit Sælensminde <kirit@felspar.com>
2026-03-22 11:57:04 +0700

    Add phi to our numbers

--- include/planet/numbers.hpp
+++ include/planet/numbers.hpp
@@ -8,6 +8,7 @@ namespace planet {
     float constexpr pi = std::numbers::pi_v<float>;
     float constexpr tau = std::numbers::pi_v<float> * 2.0f;
     float constexpr sqrt3 = std::numbers::sqrt3_v<float>;
+    float constexpr phi = std::numbers::phi_v<float>;
 }
 #else
 namespace planet {
@@ -15,6 +16,7 @@ namespace planet {
     float constexpr tau = 6.283185307179586f;
     float constexpr pi = tau / 2.0f;
     float constexpr sqrt3 = 1.7320508075688772f;
+    float constexpr phi = 1.6180339887498948f;
 }
 #endif
 
