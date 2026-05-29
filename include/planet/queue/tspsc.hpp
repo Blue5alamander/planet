1277c67f32c40cec3971e4eabd9160d355140536
Kirit Sælensminde <kirit@felspar.com>
2026-05-29 13:29:27 +0700

    The TSPC queue now gets a reserver to help reduce jitter more on start

--- include/planet/queue/tspsc.hpp
+++ include/planet/queue/tspsc.hpp
@@ -25,23 +25,39 @@ namespace planet::queue {
       public:
         using value_type = T;
 
+        /// ## Reserve capacity
+        void reserve(std::size_t const n)
+        /**
+         * Pre-allocate the internal buffers so that `push` does not allocate in
+         * steady state, which would otherwise let a producer block the consumer
+         * on a heap allocation while holding the mutex. Call before the
+         * consumer starts running.
+         */
+        {
+            std::scoped_lock _{mtx};
+            queue.reserve(n);
+            consuming.reserve(n);
+        }
+
         /// ## Push data
+        void push(value_type t)
         /**
          * Push data to the end of the queue. This process will block to acquire
          * a mutex on the internal queue.
          */
-        void push(value_type t) {
+        {
             std::scoped_lock _{mtx};
             queue.push_back(std::move(t));
         }
 
         /// ## Process data
+        std::span<value_type> consume()
         /**
          * Return data held by the queue. The process will block to acquire a
          * mutex on the internal queue. The returned `span`'s data is valid
          * until the next call to `consume`.
          */
-        std::span<value_type> consume() {
+        {
             consuming.clear();
             {
                 std::scoped_lock _{mtx};
